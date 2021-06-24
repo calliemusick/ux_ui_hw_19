2e6c6e7d6e77c48221f6eff3a8e03f8b00b3c254
Callie Musick <heycalliemusick@gmail.com>
2021-06-24 17:43:47 -0500

    Iterations
    Design iterations based on peer feedback

--- js/index.js
+++ js/index.js
@@ -5,12 +5,22 @@ $( ".theProblemCTA button" ).hover(
         $( this ).animate({
             height: '+=5px',
             width: '+=50px'
-          }); 
+          });
+        $(".theProblemCTA img").animate({
+            left: '-=25px'
+            // height: '+=5px',
+            // width: '+=50px'
+        })
     }, function() {
         $( this ).animate({
             height: '-=5px',
             width: '-=50px'
           });
+        $(".theProblemCTA img").animate({
+            left: '+=25px'
+            // height: '-=5px',
+            // width: '-=50px'
+        })
     }
 );
 
